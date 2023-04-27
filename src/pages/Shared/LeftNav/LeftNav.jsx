0b0faf5cd994a8ff385910029c5b56d9d07efd70
sakibmahbub <arafatsakib48@gmail.com>
2023-04-27 15:23:45 +0600

    displayed all categories and added dynamic route

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
   const [categories, setCategories] = useState([]);
@@ -10,7 +11,23 @@ const LeftNav = () => {
       .catch((error) => console.error(error));
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="bg-light p-4">
+      <h4 className="mb-4">All category</h4>
+      <div className="ps-3">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link
+              to={`/category/${category.id}`}
+              className="text-decoration-none text-black"
+            >
+              {category.name}
+            </Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
