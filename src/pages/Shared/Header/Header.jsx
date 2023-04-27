6bdd07f7ac312337ceb2535a2d7d43b6e2aba46b
sakibmahbub <arafatsakib48@gmail.com>
2023-04-27 13:00:55 +0600

    added header navigation

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -14,13 +14,31 @@ const Header = () => {
         </p>
         <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
-      <div className="d-flex">
+      <div className="d-flex bg-light p-4">
         <Button variant="danger">Breaking</Button>
         <Marquee className="text-danger font-weight-bold" speed={100}>
           I can be a React component, multiple React components, or just some
           text.
         </Marquee>
       </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#">About</Nav.Link>
+              <Nav.Link href="#">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#">
+                <Button variant="dark">Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </Container>
   );
 };
