055d9a1b7e0a7bd7f340ff521fc7a0f46996d2da
sakibmahbub <arafatsakib48@gmail.com>
2023-04-29 17:07:03 +0600

    created news card header

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,12 +1,28 @@
+import moment from "moment/moment";
 import React from "react";
 import { Card, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FaRegBookmark, FaShareAlt } from "react-icons/fa";
 
 const NewsCard = ({ news }) => {
   const { _id, title, details, image_url, author } = news;
   return (
     <Card className=" mb-4">
-      <Card.Header></Card.Header>
+      <Card.Header className="d-flex align-items-center">
+        <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
+        <div className="ps-2 flex-grow-1">
+          <p className="mb-0">{author?.name}</p>
+          <p>
+            <small>
+              {moment(author?.published_date).format("yyyy - MM - D")}
+            </small>
+          </p>
+        </div>
+        <div>
+          <FaRegBookmark></FaRegBookmark>
+          <FaShareAlt></FaShareAlt>
+        </div>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={image_url} />
