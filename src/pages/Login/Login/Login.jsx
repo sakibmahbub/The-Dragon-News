8e7878e79b182935712d71b29ebe02ab49fa60ee
sakibmahbub <arafatsakib48@gmail.com>
2023-04-30 10:49:07 +0600

    redirect to route from the login page

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useContext } from "react";
 import { Button, Container, Form } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 
 const Login = () => {
   const { loginUser } = useContext(AuthContext);
   const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/category/0";
 
   const loginHandle = (event) => {
     event.preventDefault();
@@ -19,7 +21,7 @@ const Login = () => {
       .then((result) => {
         const loggedUser = result.user;
         console.log(loggedUser);
-        navigate("/category/0");
+        navigate(from, { replace: true });
       })
       .catch((error) => console.log(error));
   };
