c5c5b41166ddb1d86ae1bac9017da1ef5b0e8787
sakibmahbub <arafatsakib48@gmail.com>
2023-05-01 00:59:22 +0600

    fixed private route reload and added spinner

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -14,22 +14,27 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const loginUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   const logOutUser = () => {
+    setLoading(true);
     return signOut(auth);
   };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
       setUser(loggedUser);
+      setLoading(false);
     });
 
     return () => {
@@ -42,6 +47,7 @@ const AuthProvider = ({ children }) => {
     createUser,
     loginUser,
     logOutUser,
+    loading,
   };
 
   return (
